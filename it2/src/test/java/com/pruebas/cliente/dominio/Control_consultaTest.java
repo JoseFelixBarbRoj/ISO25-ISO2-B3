b59e7460acee4ed4d41078cb8f6558b509a2e409
Rafags04 <rafags2004@gmail.com>
2025-11-12 12:27:21 +0100

    Test it2

---
+++ it2/src/test/java/com/pruebas/cliente/dominio/Control_consultaTest.java
@@ -0,0 +1,12 @@
+package com.pruebas.cliente.dominio;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Control_consultaTest {
+        @Test
+    public void Control_consultatest() {
+        assertEquals(42, 40 + 2);
+    }
+}
---
+++ it2/src/test/java/com/pruebas/servidor/persistencia/AgenteTest.java
@@ -0,0 +1,12 @@
+package com.pruebas.servidor.persistencia;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AgenteTest {
+            @Test
+    public void Agentetest() {
+        assertEquals(42, 40 + 2);
+    }
+}
