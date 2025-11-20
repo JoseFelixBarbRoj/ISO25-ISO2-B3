9edc1931afda49f5448d1b76fd656b9ef9a04dcf
Jose-Antonio-Estevez-Lopez <joseantonio.estevez1@alu.uclm.es>
2025-11-20 13:32:27 +0100

    Creada la iteración 4 del proyecto ISO2

---
+++ it4/src/main/java/iso2/cliente/Dominio/Control_renovacion.java
@@ -0,0 +1,15 @@
+package iso2.cliente.Dominio;
+
+public class Control_renovacion {
+
+	/**
+	 * 
+	 * @param id_reserva
+	 * @param datos_nuevos
+	 */
+	public boolean guardarDatosRenovacion(int id_reserva, String datos_nuevos) {
+		// TODO - implement Control_renovacion.guardarDatosRenovacion
+		throw new UnsupportedOperationException();
+	}
+
+}
