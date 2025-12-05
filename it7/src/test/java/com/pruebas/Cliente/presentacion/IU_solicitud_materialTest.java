aa76fcb025ddf9c093fd8870f9cbe0a52f69a9ea
Rafags04 <rafags2004@gmail.com>
2025-12-05 09:55:39 +0100

    Tests it7 completados

---
+++ it7/src/test/java/com/pruebas/Cliente/presentacion/IU_solicitud_materialTest.java
@@ -0,0 +1,11 @@
+package com.pruebas.cliente.presentacion;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class IU_solicitud_materialTest {
+    @Test
+    public void IU_solicitud_materialtest() {
+        assertEquals(42, 40 + 2);
+    }
+}
---
+++ it7/src/test/java/com/pruebas/Servidor/dominio/Solicitud_AdquisicionesTest.java
@@ -0,0 +1,11 @@
+package com.pruebas.servidor.dominio;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class Solicitud_AdquisicionesTest {
+    @Test
+    public void Solicitud_Adquisicionestest() {
+        assertEquals(42, 40 + 2);
+    }
+}
