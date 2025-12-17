26432104f8b45e031804f3342d97be564af1269f
Rafags04 <rafags2004@gmail.com>
2025-12-17 13:47:01 +0100

    Tests it10 completados

---
+++ it10/src/test/java/com/pruebas/cliente/Dominio/UsuarioTest.java
@@ -0,0 +1,10 @@
+package com.pruebas.cliente.dominio;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class UsuarioTest {
+    @Test
+    public void Usuariotest() {
+        assertEquals(42, 40 + 2);
+    }
+}
